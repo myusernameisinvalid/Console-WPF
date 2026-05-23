37195e1c3609e449e85fd368e6d4d3ecd7a9b8c6
myusernameisinvalid <90838645+myusernameisinvalid@users.noreply.github.com>
2026-05-23 22:46:09 +0200

    Add files via upload

---
+++ LogManager/Logok.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogManager
+{
+    internal class Logok
+    {
+        public DateTime Idopont { get; set; }
+        public string Szint { get; set; }
+        public string Uzenet { get; set; }
+
+        public Logok(string sor)
+        {
+            string[] s = sor.Split(new string[] { " - " }, StringSplitOptions.None);
+            this.Idopont = Convert.ToDateTime(s[0]);
+            this.Szint = s[1];
+            this.Uzenet = s[2];
+        }
+
+        public override string ToString()
+        {
+            return $"{Idopont:yyyy-MM-dd HH:mm:ss} - {Szint} - {Uzenet}";
+        }
+    }
+}
---
+++ LogManager/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Linq;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LogManagerWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Logok> logok = new ObservableCollection<Logok>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            lbLogok.ItemsSource = logok;
+
+            try
+            {
+                string[] sorok = File.ReadAllLines("server.log");
+                foreach (var sor in sorok)
+                {
+                    logok.Add(new Logok(sor));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hiba történt a fájl betöltésekor:\n{ex.Message}", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        //Az "Előző" gomb megnyomásával a program az előző, megfelelő szintű bejegyzést jeleníti meg.
+        private void elozo(object sender, RoutedEventArgs e)
+        {
+            string keresettSzint = GetKivalasztottSzint();
+            int jelenlegiIndex = lbLogok.SelectedIndex;
+            if (jelenlegiIndex == -1)
+            {
+                jelenlegiIndex = logok.Count;
+            }
+            for (int i = jelenlegiIndex - 1; i >= 0; i--)
+            {
+                if (logok[i].Szint == keresettSzint)
+                {
+                    lbLogok.SelectedIndex = i;
+                    lbLogok.ScrollIntoView(lbLogok.SelectedItem);
+                    return;
+                }
+            }
+
+            MessageBox.Show($"Nincs több '{keresettSzint}' szintű bejegyzés felfelé!", "Infó", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        //A "Következő" gomb megnyomásával a program a következő olyan bejegyzést jeleníti meg, amely megfelel a kiválasztott szintnek.
+        private void kovetkezo(object sender, RoutedEventArgs e)
+        {
+            string keresettSzint = GetKivalasztottSzint();
+            int jelenlegiIndex = lbLogok.SelectedIndex;
+            for (int i = jelenlegiIndex + 1; i < logok.Count; i++)
+            {
+                if (logok[i].Szint == keresettSzint)
+                {
+                    lbLogok.SelectedIndex = i;
+                    lbLogok.ScrollIntoView(lbLogok.SelectedItem);
+                    return;
+                }
+            }
+            MessageBox.Show($"Nincs több '{keresettSzint}' szintű bejegyzés lefelé!", "Infó", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        private string GetKivalasztottSzint()
+        {
+            ComboBoxItem kivalasztottElem = (ComboBoxItem)cbSzintek.SelectedItem;
+            return kivalasztottElem.Content.ToString();
+        }
+    }
+}
