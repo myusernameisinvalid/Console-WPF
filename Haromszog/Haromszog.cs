b1ff53485da248e36833ab0ecaf04deee4c6e78e
myusernameisinvalid <90838645+myusernameisinvalid@users.noreply.github.com>
2026-05-23 20:09:26 +0200

    Add files via upload

---
+++ Haromszog/Haromszog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace haromszogekCLI
+{
+    public class Haromszog
+    {
+        public int a { get;private set; }
+        public int b { get; private set; }
+        public int c { get; private set; }
+
+        public Haromszog(string sor)
+        {
+            string[] adatok = sor.Split(' ');
+            this.a = int.Parse(adatok[0]);
+            this.b = int.Parse(adatok[1]);
+            this.c = int.Parse(adatok[2]);
+        }
+
+        /*A program tartalmazzon egy függvényt amely eldönti egy háromszögről, hogy
+        derékszögű-e! A függvény megkap egy háromszög példányt, vagy annak oldalait, és
+        eldönti róla, hogy derékszögű e!
+        Egy háromszög derékszögű ha teljesül rá a Pitagorasz-tétele:. a 2 +b 2 =c 2 ,
+        Mivel az adatok növekvő sorrendben vannak így az utolsó érték lehet csak az átfogó.
+        Ez alapján könnyen ellenőrizhető, hogy a háromszög derékszögű-e.
+        A függvény igaz logikai értékkel tér vissza, amennyiben a háromszög derékszögű! Hamissal, ha nem az!
+        */
+        public bool derekszogu(int a,int b,int c)
+        {
+            if ((a * a) + (b * b) == (c * c))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Haromszog/HaromszogTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using haromszogekCLI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace haromszogekCLI.Tests
+{
+    [TestClass()]
+    public class HaromszogTests
+    {
+        [TestMethod()]
+        public void derekszoguTest()
+        {
+            //A teszttel ellenőrizze le, hogy a=3 b=4 c=5 oldalaknál a függvény helyes(true) értékkel tér-e vissza!
+            string tesztadat = "3 4 5";
+            Haromszog tesztsor = new Haromszog(tesztadat);
+            bool kapotteredmeny = tesztsor.derekszogu(3,4,5);
+            Assert.IsTrue(kapotteredmeny);
+        }
+    }
+}
