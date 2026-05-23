81156d933d57a4adf9abe150e943f14d27eb911b
myusernameisinvalid <90838645+myusernameisinvalid@users.noreply.github.com>
2026-05-23 20:21:40 +0200

    Add files via upload

---
+++ Csudh/Szerver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csudh
+{
+    public class Szerver
+    {
+        public string Domaincim { get;private set; }
+        public string Ip { get; private set; }
+
+        public Szerver(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            this.Domaincim = adatok[0];
+            this.Ip = adatok[1];
+        }
+
+        /*Készítsen metódust vagy függvényt Domain azonosítóval, amely visszatér a domainnév megadott szintű részével! 
+        A szintet (1..5) a függvény paraméterében lehessen megadni. A legfelső szintű domaint (top level domain) az 1-es érték jelölje! 
+        Az IP-cím a függvény paramétere vagy metódus esetén az osztály adattagja legyen! 
+        Ha nem létezik a megadott szintű név, akkor a „nincs” értékkel térjen vissza az alprogram!
+        */
+        public string Domain(int szint)
+        {
+            string[] reszek = this.Domaincim.Split('.');
+            if (szint > reszek.Length)
+            {
+                return "Nincs";
+            }
+            else
+            {
+                int index = reszek.Length - szint;
+                return reszek[index];
+            }
+        }
+    }
+}
---
+++ Csudh/SzerverTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using csudh;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csudh.Tests
+{
+    [TestClass()]
+    public class SzerverTests
+    {
+        //Készítsen tesztet a függvényhez! A teszt vizsgáljon hibás és nem hibás eredményeket is!
+        [TestMethod()]
+        public void DomainTest()
+        {
+            Szerver tesztAdat = new Szerver("dhvx.csudh.edu;155.135.1.2");
+            string elsoszint = tesztAdat.Domain(1);
+            string masodikszint = tesztAdat.Domain(2);
+            string harmadikszint = tesztAdat.Domain(3);
+
+            Assert.AreEqual("edu", elsoszint);
+            Assert.AreEqual("csudh", masodikszint);
+            Assert.AreEqual("dhvx", harmadikszint);
+        }
+        [TestMethod()]
+        public void DomainTest2() 
+        {
+            Szerver tesztSzerver = new Szerver("dhvx20.csudh.edu;155.135.1.1");
+            string negyedikSzint = tesztSzerver.Domain(4);
+            string otodikSzint = tesztSzerver.Domain(5);
+
+            Assert.AreEqual("Nincs", negyedikSzint);
+            Assert.AreEqual("Nincs", otodikSzint);
+        }
+
+    }
+}
